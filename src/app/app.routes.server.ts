1c47058d120a1a6c3f89d1ba4c7bfa234b11d135
jrgm-fss-ap-13 <jrgm.fss.ap@gmail.com>
2026-04-01 01:27:08 -0300

    fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+    {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
